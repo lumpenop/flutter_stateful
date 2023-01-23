f77c2d7fa00a3699c7cc5008a7018c6147a93eab
lumpenop <swk5373@gmail.com>
2023-01-23 23:58:41 +0900

    + 코드 삭제 후, 위젯 분리

--- lib/main.dart
+++ lib/main.dart
@@ -13,42 +13,39 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
-  int counter = 0;
-  void onClicked() {
-    setState(() {
-      counter = counter + 1;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+          textTheme: const TextTheme(
+              titleLarge: TextStyle(
+        color: Colors.red,
+      ))),
       title: 'Flutter Demo',
       home: Scaffold(
         backgroundColor: const Color(0x0ff4eddb),
         body: Center(
             child: Column(
-          children: [
-            const SizedBox(
-              height: 100,
-            ),
-            const Text(
-              'click',
-              style: TextStyle(color: Colors.white),
-            ),
-            Text(
-              '$counter',
-              style: const TextStyle(color: Colors.white),
-            ),
-            IconButton(
-              iconSize: 40,
-              color: Colors.white,
-              onPressed: onClicked,
-              icon: const Icon(Icons.add_box_rounded),
-            ),
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            LargeTitle(),
           ],
         )),
       ),
+    );
+  }
+}
+
+class LargeTitle extends StatelessWidget {
+  const LargeTitle({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      'Large Title',
+      style: TextStyle(color: Colors.white, fontSize: 30),
     );
   }
 }
